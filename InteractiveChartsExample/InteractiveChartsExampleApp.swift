a472d50b352dc8d4a248db02208bf019d0d26ed9
katafuchix <katafuchix@gmail.com>
2024-01-31 17:23:33 +0900

    first commit

---
+++ InteractiveChartsExample/InteractiveChartsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveChartsExampleApp.swift
+//  InteractiveChartsExample
+//
+//  Created by cano on 2024/01/31.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveChartsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
